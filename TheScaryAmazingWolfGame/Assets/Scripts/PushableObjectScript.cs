56ba4b090e0cab4d3743c59e968ee7314346f5d9
Joanna <joanna.b.nilsson@live.se>
2024-07-31 15:01:08 +0200

    JoannaScene
    Lagt till Player i min scen för testning

--- TheScaryAmazingWolfGame/Assets/Scripts/PushableObjectScript.cs
+++ TheScaryAmazingWolfGame/Assets/Scripts/PushableObjectScript.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PushableObjectScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Rigidbody2D rb2d;
+    
+    private void OnCollision2D(Collider2D collision)
     {
-        
-    }
+        if (collision.gameObject.CompareTag("Player"))
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
+           rb2d = GetComponent<Rigidbody2D>();
+        }
+
        
     }
 }
